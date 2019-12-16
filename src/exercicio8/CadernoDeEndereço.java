88c8b14f7e2918e4a3d13f19b18efa2f9ae0d276
nmmuchate <nicolas.muchate@gmail.com>
2019-12-16 08:55:27 +0200

    exercicio8,atributos,getters and setters

---
+++ src/exercicio8/CadernoDeEndereço.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio8;
+
+/**
+ *
+ * @author Vagabundo
+ */
+public class CadernoDeEndereço {
+    private String nome;
+    private int numerodetelefone;
+    private String email;
+    private String endereço;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the numerodetelefone
+     */
+    public int getNumerodetelefone() {
+        return numerodetelefone;
+    }
+
+    /**
+     * @param numerodetelefone the numerodetelefone to set
+     */
+    public void setNumerodetelefone(int numerodetelefone) {
+        this.numerodetelefone = numerodetelefone;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the endereço
+     */
+    public String getEndereço() {
+        return endereço;
+    }
+
+    /**
+     * @param endereço the endereço to set
+     */
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+    
+    
+}
