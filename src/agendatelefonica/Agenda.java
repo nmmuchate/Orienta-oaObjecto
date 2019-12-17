6cc2353a362f7eb055eb92b2fb5752883323e4c4
nmmuchate <nicolas.muchate@gmail.com>
2019-12-17 12:05:22 +0200

    classe agenda terminada

--- src/agendatelefonica/Agenda.java
+++ src/agendatelefonica/Agenda.java
@@ -12,6 +12,20 @@ import java.util.*;
  * @author Vagabundo
  */
 public class Agenda {
-    
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Telefone contactos = new Telefone();
+        Vector dad = new Vector();
+        System.out.println("Insira Os Contactos.");
+        System.err.println("Mas tenha cuidado tem um limete de contactos");
+        for (int i = 0; i < 10; i++) {
+            contactos.setNome(sc.next());
+            contactos.setNúmero(sc.nextInt());
+            
+            dad.add(contactos);
+        }
+        
+        System.out.println(dad);
+    }
+     
 }
