24061cd6c53f26385dcedf55d692076fad1b4bde
hassan mutola <hassanmx57@gmail.com>
2019-12-19 00:25:28 +0200

    esta tudo a funcionar perfeitamente

--- src/hassanexercicios/canetaa.java
+++ src/hassanexercicios/canetaa.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hassanexercicios;
 
 /**
@@ -10,5 +6,28 @@ package hassanexercicios;
  * @author Hassan
  */
 public class canetaa {
-    
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    protected boolean tampada;
+    public void status () {
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("uma caneta" + this.cor);
+        System.out.println("carga: "+ this.carga);
+        System.out.println("esta tampada?"+ this.tampada);
+    }
+    public void rabiscar () {
+     if (this.tampada == true){
+         System.out.println("ERRO! NAO POSSO RABISCAR");
+     }  else{
+         System.out.println("estou rabiscando");
+     } 
+    }
+    protected void tampar () {
+     this.tampada=true;   
+    }
+    protected void destampar (){
+      this.tampada=false; 
+    }
 }
