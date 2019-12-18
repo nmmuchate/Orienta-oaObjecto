ea26afde140949047c92c3e9964172c997cb9047
hassan mutola <hassanmx57@gmail.com>
2019-12-18 23:33:32 +0200

    exercicio sobre caneta a ser concluido

--- src/hassanexercicios/caneta.java
+++ src/hassanexercicios/caneta.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hassanexercicios;
 
 /**
@@ -10,5 +6,22 @@ package hassanexercicios;
  * @author Hassan
  */
 public class caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+    void status () {
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("uma caneta" + this.cor);
+        System.out.println("carga: "+ this.carga);
+        System.out.println("esta tampada?"+ this.tampada);
+    }
+    void rabiscar () {
+        
+    }
+    void tampar () {
+        
+    }
     
 }
--- src/hassanexercicios/poo2.java
+++ src/hassanexercicios/poo2.java
@@ -9,6 +9,9 @@ public class poo2 {
     public static void main(String[] args) {
         caneta c1 = new caneta();
         c1.cor = "Azul";
+        c1.ponta = 0.5f;
+        c1.tampada = false;
+        c1.status();
     }
  
 }
