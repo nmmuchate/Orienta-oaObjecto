23a367eabf7bb9e1289cd98fc93f22897498bc85
hassan mutola <hassanmx57@gmail.com>
2019-12-19 00:29:05 +0200

    colocamos a cor azul e funcionou

--- src/hassanexercicios/aula2.java
+++ src/hassanexercicios/aula2.java
@@ -9,6 +9,7 @@ public class aula2 {
     public static void main(String[] args) {
         caneta c1 = new caneta ();
         c1.modelo="BIC cristal";
+        c1.cor = "Azul";
         c1.status();
     }
  
