683bd1c56387726ce2f6d397940d84cbd9803937
nmmuchate <nicolas.muchate@gmail.com>
2019-12-18 07:46:14 +0200

    ex11, classe circulo, construtores e getts/setts

---
+++ src/exercicio11/Circulo.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio11;
+
+/**
+ *
+ * @author Vagabundo
+ */
+public class Circulo {
+    private double x;
+    private double y;
+    private double PI;
+
+    /**
+     * @return the x
+     */
+    public double getX() {
+        return x;
+    }
+
+    /**
+     * @param x the x to set
+     */
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    /**
+     * @return the y
+     */
+    public double getY() {
+        return y;
+    }
+
+    /**
+     * @param y the y to set
+     */
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    /**
+     * @return the PI
+     */
+    public double getPI() {
+        return PI;
+    }
+
+    /**
+     * @param PI the PI to set
+     */
+    public void setPI(double PI) {
+        this.PI = PI;
+    }
+    
+    
+    
+    
+}
