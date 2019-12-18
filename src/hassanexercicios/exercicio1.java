3b64268077175b94ef0e564e6f5d8a42e0346cde
hassan mutola <hassanmx57@gmail.com>
2019-12-18 22:13:13 +0200

    alguns testes feitos

---
+++ src/hassanexercicios/exercicio1.java
@@ -0,0 +1,13 @@
+
+package hassanexercicios;
+
+/**
+ *
+ * @author Hassan
+ */
+public class exercicio1 {
+    public static void main(String[] args) {
+    
+        System.out.println("ola mundo");
+    }
+}
