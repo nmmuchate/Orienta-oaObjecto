5556058a85e215082b6d3281399b625d3744423a
hassan mutola <hassanmx57@gmail.com>
2019-12-18 23:45:40 +0200

    ele passou o erro pra baixo

--- src/hassanexercicios/poo2.java
+++ src/hassanexercicios/poo2.java
@@ -11,8 +11,9 @@ public class poo2 {
         c1.cor = "Azul";
         c1.ponta = 0.5f;
         c1.tampar();
-        c1.rabiscar();
+        
         c1.status(); 
+        c1.rabiscar();
     }
  
 }
