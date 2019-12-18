d4f27a4b92f9325b08c6aef5b2fac6385ce86f77
nmmuchate <nicolas.muchate@gmail.com>
2019-12-18 09:33:49 +0200

    ex4,classe pessoa,construtor,getters/settters

---
+++ src/exercicio4/Pessoa.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio4;
+
+/**
+ *
+ * @author Vagabundo
+ */
+public class Pessoa {
+    private String nome;
+    private int bi;
+    private String email;
+    private String numerodecelular;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the bi
+     */
+    public int getBi() {
+        return bi;
+    }
+
+    /**
+     * @param bi the bi to set
+     */
+    public void setBi(int bi) {
+        this.bi = bi;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the numerodecelular
+     */
+    public String getNumerodecelular() {
+        return numerodecelular;
+    }
+
+    /**
+     * @param numerodecelular the numerodecelular to set
+     */
+    public void setNumerodecelular(String numerodecelular) {
+        this.numerodecelular = numerodecelular;
+    }
+    
+}
